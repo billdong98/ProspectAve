6d9dad2cd114387350f4950d0d12f7b38d782ed3
Michael Man <17michaelm@gmail.com>
2018-04-13 21:24:15 -0400

    Added CAS authentication

---
+++ node app/Auth.js
@@ -0,0 +1,50 @@
+const https = require('https');
+
+
+// verifies the ticketID passed back to /login via CAS
+const serviceURL = 'https://prospectave.io/redirect/';
+function verify(ticketID, callback){
+    console.log("Verifying");
+    //console.log(`https://fed.princeton.edu/cas/validate?service=${serviceURL}&ticket=${ticketID}`);
+    let validateURL = `https://fed.princeton.edu/cas/validate?service=${serviceURL}&ticket=${ticketID}`
+    https.get(validateURL,
+              (res,d)=>
+              {
+        let body = "";
+        res.setEncoding("utf8");
+        res.on('data',(d) => {body+=d})
+        res.on('end', ()=>{
+            let answer = body.split('\n');
+            if (answer[0] == 'no'){
+                callback(false);
+            }
+            callback(answer[1]); //return netID
+        })}).on('error',console.log);
+}
+
+
+//const serviceURL = 'https://localhost:1738/login';
+
+function redirect(response){
+    response.writeHead(301,
+                       {Location: `https://fed.princeton.edu/cas/login?service=https://prospectave.io/redirect/`}
+                      );
+    response.end();
+}
+
+
+function redirectOfficer(response){
+        response.writeHead(301,
+                       {Location: `https://fed.princeton.edu/cas/login?service=https://prospectave.io/officer.html`}
+                      );
+    response.end();
+}
+
+
+module.exports = {
+    redirect : function(res){ redirect(res)},
+    verify: function(ticketID, callback){verify(ticketID, callback)},
+    redirectOfficer: function(res){ redirectOfficer(res)}
+}
+
+
--- node app/server.js
+++ node app/server.js
@@ -9,11 +9,20 @@ const sqlite3 = require('sqlite3').verbose();
 const bodyParser = require('body-parser');
 const dateTime = require('node-datetime');
 
+const auth = require('./Auth.js');
 
+// for handling sessions
+var cookieSession = require('cookie-session');
 
 // setup Express server
 app.use(bodyParser.json());
 app.use(cors());
+app.use(cookieSession({
+    name: 'prospectave_session',
+    secret: 'verysecurekey',
+    // Cookie Options
+    maxAge: 24 * 60 * 60 * 1000 // 24 hours
+}))
 
 var https = require('https');
 //https.createServer(options, app).listen(1234);
@@ -21,19 +30,20 @@ var https = require('https');
 
 /* handling HTTPS */
 const options = {
-  key: fs.readFileSync('./../../ssl/keys/9f016_d0a13_46664a744423c921776ce2dde252b120.key').toString(),
-  cert: fs.readFileSync('./../../ssl/certs/prospectave_io_9f016_d0a13_1530835199_f77b28e7908bb77ad4e6cbe0ea03205e.crt').toString()
+    key: fs.readFileSync('./../../ssl/keys/9f016_d0a13_46664a744423c921776ce2dde252b120.key').toString(),
+    cert: fs.readFileSync('./../../ssl/certs/prospectave_io_9f016_d0a13_1530835199_f77b28e7908bb77ad4e6cbe0ea03205e.crt').toString()
 };
 
 https.createServer(options, app).listen(1738);
 console.log("Server listening on port: " + 1738);
+console.log("Current date: " + currentDate());
 
 // connect to the db file
 let db = new sqlite3.Database('./clubs.db', (err) => {
-  if (err) {
-    return console.error(err.message);
-  }
-  console.log('Connected to the SQLite DB.');
+    if (err) {
+        return console.error(err.message);
+    }
+    console.log('Connected to the SQLite DB.');
 });
 
 
@@ -48,12 +58,12 @@ let placeholders = '(?,?,?,?,?,?)';
 /* gets the current clubs from the DB */
 app.get('/status', (request, response) => { 
     console.log('Hello getter! Current date: ' + currentDate());
-        
+
     db.all(selectAll, [], (err, rows) => {
         if(err){
             throw err;
         }
-        
+
         console.log("GET: sent " + rows.length + " rows.");
         response.send(rows);
     });
@@ -63,19 +73,81 @@ app.get('/status', (request, response) => {
 /* clears the DB */
 app.get('/clear', (request, response) => { 
     console.log('CLEARING DB');
-    
+
     db.run("DELETE from club_status", [], (err, result) => {
         if(err){
             throw err;
         }
-        
+
         console.log(result);
         response.send(result);
     });
 })
 
+app.get('/login', (request, response) => { 
+    console.log('Login');
+    
+    var callback = function(result){
+            if(result != false){
+                request.session.id = result;
+                console.log("Created cookie for: " + request.session.id);
+                response.send("Sup " + result);
+            } else {
+                response.status(500);
+                response.send("Bad auth token.");
+            }
+        }.bind({request: request, response: response});
+    
+    if(request.session.isPopulated) {
+        console.log("Already logged in");
+        console.log(`Redirecting: ${request.session.id}`);
+        auth.redirectOfficer(response);
+        // redirect to officer page
+        return;
+    }
+    
+    // no ticket, need to authenticate
+    if(!request.query.ticket){
+        console.log("Redirecting to CAS");
+        auth.redirect(response);
+    } else {
+        // we have just been redirected back here by CAS
+        auth.verify(request.query.ticket, callback); 
+    }
+})
 
-/* handles a post request */
+
+// sends netID and club information as JSON to officer.html
+app.get('/userinfo', (request, response) => { 
+    response.setHeader('Content-Type', 'application/json');
+        
+    var data = {netID: null, club: "TestClub"};
+    if(!request.session.isPopulated) {
+        data.club(null);
+        console.log("Not logged in.");
+    } else {
+        console.log("Welcoming, " + request.session.id);
+        data.netID = request.session.id;
+    }
+    
+    response.json(data);
+});
+
+
+
+app.get('/testlogin', (request, response) => { 
+    console.log('Testing login');
+
+    if(!request.session.isPopulated){
+        response.end("Not logged in");
+    } else {
+        response.end(`Welcome, ${request.session.id}`);
+    }
+});
+
+
+
+/* handles a post request of club data */
 app.post('/officer_post', (request, response) => {
     var obj = request.body;
     // IMPLEMENT THIS FOR JSON ARRAY LATER
@@ -86,10 +158,10 @@ app.post('/officer_post', (request, response) => {
     var status = obj.s;
     var post_date = currentDate();
     var info = obj.i;
-    
+
     var data = [];
     var query = post;
-    
+
     // construct query and input for each element in dates
     for(var i=0; i<dates.length;i++){
         if(i != 0){
@@ -99,19 +171,19 @@ app.post('/officer_post', (request, response) => {
         var date = dates[i];
         data.push(club, date, netID, post_date, status, info);
     }
-    
+
     console.log(query);
     console.log(data);
-    
+
     db.run(query, data, (err)=>{
-       if(err){ 
-           response.status(500);
-           response.send("ERROR");
-           console.log(err);
-       } else {
-           console.log("CHANGED: " + this.changes);
-           response.send("Ok.");
-       }
+        if(err){ 
+            response.status(500);
+            response.send("ERROR");
+            console.log(err);
+        } else {
+            console.log("CHANGED: " + this.changes);
+            response.send("Ok.");
+        }
     });
 })
 
@@ -119,6 +191,5 @@ app.post('/officer_post', (request, response) => {
 // returns the current time in 
 function currentDate(){
     var dt = dateTime.create();
-    var formatted = dt.format('m/d/Y');
-    return formatted;
+    return dt.format('m/d/Y');
 }
