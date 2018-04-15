4dbef479b1ccb9d09d580e5ca53eea5cf93ff94a
Ben Liang <bliang@princeton.edu>
2018-04-14 22:42:47 -0400

    Connected feedback form to email maybe?

---
+++ webapp/send_mail.php
@@ -0,0 +1,42 @@
+<?php
+/*
+This first bit sets the email address that you want the form to be submitted to.
+You will need to change this value to a valid email address that you can access.
+*/
+$webmaster_email = "prospectaveio@gmail.com";
+
+/*
+This next bit loads the form field data into variables.
+*/
+$comments = $_REQUEST['feedback'] ;
+
+/*
+The following function checks for email injection.
+Specifically, it checks for carriage returns - typically used by spammers to inject a CC list.
+*/
+function isInjected($str) {
+	$injections = array('(\n+)',
+	'(\r+)',
+	'(\t+)',
+	'(%0A+)',
+	'(%0D+)',
+	'(%08+)',
+	'(%09+)'
+	);
+	$inject = join('|', $injections);
+	$inject = "/$inject/i";
+	if(preg_match($inject,$str)) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+// If we passed all tests, send the email.
+if (!isInjected($comments)) {
+
+	mail( "$webmaster_email", "Feedback Form Results", $comments );
+	header( "Location: $feedback_page" );
+}
+?>
