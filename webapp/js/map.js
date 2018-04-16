7ec3935e11b1e5ea7da9b14c2a1b1bc5a966c501
Ben Liang <bliang@princeton.edu>
2018-04-16 19:12:00 -0400

    Changed date display

--- webapp/js/map.js
+++ webapp/js/map.js
@@ -3,13 +3,13 @@ window.data = {};
 window.date;
 var dateDisplay;
 var weekdays = new Array(7);
-weekdays[0] = "Sunday";
-weekdays[1] = "Monday";
-weekdays[2] = "Tuesday";
-weekdays[3] = "Wednesday";
-weekdays[4] = "Thursday";
-weekdays[5] = "Friday";
-weekdays[6] = "Saturday";
+weekdays[0] = "Sun";
+weekdays[1] = "Mon";
+weekdays[2] = "Tue";
+weekdays[3] = "Wed";
+weekdays[4] = "Thu";
+weekdays[5] = "Fri";
+weekdays[6] = "Sat";
 
 let clubs = ["terrace", "tower", "colonial", "cannon", "quadrangle", "ti", "ivy","cottage", "cap", "cloister", "charter"];
 
