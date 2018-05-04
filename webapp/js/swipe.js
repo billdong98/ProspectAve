95db0b60e028d64c07bb5d3687be67e516ca8f6b
Michael Man <17michaelm@gmail.com>
2018-05-04 04:40:01 -0400

    ┌∩┐(◣_◢)┌∩┐

--- webapp/js/swipe.js
+++ webapp/js/swipe.js
@@ -1,6 +1,6 @@
 // TOUCH-EVENTS SINGLE-FINGER SWIPE-SENSING JAVASCRIPT
 // Courtesy of PADILICIOUS.COM and MACOSXAUTOMATION.COM
-	
+
 // this script can be used with one or more page elements to perform actions based on them being swiped with a single finger
 
 var triggerElementID = null; // this variable is used to identity the triggering element
@@ -17,126 +17,127 @@ var minLength = 72; // the shortest distance the user may swipe
 var swipeLength = 0;
 var swipeAngle = null;
 var swipeDirection = null;
-	
+
 // The 4 Touch Event Handlers
-	
+
 // NOTE: the touchStart handler should also receive the ID of the triggering element
 // make sure its ID is passed in the event call placed in the element declaration, like:
 // <div id="picture-frame" ontouchstart="touchStart(event,'picture-frame');"  ontouchend="touchEnd(event);" ontouchmove="touchMove(event);" ontouchcancel="touchCancel(event);">
 
 function touchStart(event,passedName) {
-	// disable the standard ability to select the touched object
-	event.preventDefault();
-	// get the total number of fingers touching the screen
-	fingerCount = event.touches.length;
-	// since we're looking for a swipe (single finger) and not a gesture (multiple fingers),
-	// check that only one finger was used
-	if ( fingerCount == 1 ) {
-		// get the coordinates of the touch
-		startX = event.touches[0].pageX;
-		startY = event.touches[0].pageY;
-		// store the triggering element ID
-		triggerElementID = passedName;
-	} else {
-		// more than one finger touched so cancel
-		touchCancel(event);
-	}
+    // disable the standard ability to select the touched object
+    event.preventDefault();
+    // get the total number of fingers touching the screen
+    fingerCount = event.touches.length;
+    // since we're looking for a swipe (single finger) and not a gesture (multiple fingers),
+    // check that only one finger was used
+    if ( fingerCount == 1 ) {
+        // get the coordinates of the touch
+        startX = event.touches[0].pageX;
+        startY = event.touches[0].pageY;
+        // store the triggering element ID
+        triggerElementID = passedName;
+    } else {
+        // more than one finger touched so cancel
+        touchCancel(event);
+    }
 }
 
 function touchMove(event) {
-	event.preventDefault();
-	if ( event.touches.length == 1 ) {
-		curX = event.touches[0].pageX;
-		curY = event.touches[0].pageY;
-		deltaX = curX - startX;
-		if (deltaX < 0 && -1 * deltaX < minLength) {
-				var infobar_mobile = document.getElementById("infobar_mobile");
-				infobar_mobile.style.left= deltaX*0.2 + "vw";
-		}
-	} else {
-		touchCancel(event);
-	}
+    event.preventDefault();
+    if ( event.touches.length == 1 ) {
+        curX = event.touches[0].pageX;
+        curY = event.touches[0].pageY;
+        deltaX = curX - startX;
+        if (deltaX < 0 && -1 * deltaX < minLength) {
+            var infobar_mobile = document.getElementById("infobar_mobile");
+            infobar_mobile.style.left= deltaX*0.2 + "vw";   
+        }
+    } else {
+        touchCancel(event);
+    }
 }
-	
+
 function touchMoveDate(event) {
-	event.preventDefault();
-	if ( event.touches.length == 1 ) {
-		curX = event.touches[0].pageX;
-		curY = event.touches[0].pageY;
-		deltaX = curX - startX;
-		var mobile_bar = document.getElementById("mobile_bar");
-		mobile_bar.style.backgroundColor = "#c3e4ef";
-	} else {
-		touchCancel(event);
-	}
+    event.preventDefault();
+    if ( event.touches.length == 1 ) {
+        curX = event.touches[0].pageX;
+        curY = event.touches[0].pageY;
+        deltaX = curX - startX;
+        var mobile_bar = document.getElementById("mobile_bar");
+        
+        // DO NOTHING?
+    } else {
+        touchCancel(event);
+    }
 }	
 
 function touchEnd(event) {
-	event.preventDefault();
-	// check to see if more than one finger was used and that there is an ending coordinate
-	if ( fingerCount == 1 && curX != 0 ) {
-		// use the Distance Formula to determine the length of the swipe
-		swipeLength = Math.round(Math.sqrt(Math.pow(curX - startX,2) + Math.pow(curY - startY,2)));
-		// if the user swiped more than the minimum length, perform the appropriate action
-		if ( swipeLength >= minLength ) {
-			caluculateAngle();
-			determineSwipeDirection();
-			processingRoutine();
-			touchCancel(event); // reset the variables
-		} else {
-			if (triggerElementID=='infobar_mobile') {
-				var infobar_mobile = document.getElementById("infobar_mobile");
-				infobar_mobile.style.left="0vw"; 
-			}
-			touchCancel(event);
-		}	
-	} else {
-		touchCancel(event);
-	}
+    event.preventDefault();
+    // check to see if more than one finger was used and that there is an ending coordinate
+    if ( fingerCount == 1 && curX != 0 ) {
+        // use the Distance Formula to determine the length of the swipe
+        swipeLength = Math.round(Math.sqrt(Math.pow(curX - startX,2) + Math.pow(curY - startY,2)));
+        // if the user swiped more than the minimum length, perform the appropriate action
+        if ( swipeLength >= minLength ) {
+            caluculateAngle();
+            determineSwipeDirection();
+            processingRoutine();
+            touchCancel(event); // reset the variables
+        } else {
+            if (triggerElementID=='infobar_mobile') {
+                var infobar_mobile = document.getElementById("infobar_mobile");
+                infobar_mobile.style.left="0vw"; 
+            }
+            touchCancel(event);
+        }	
+    } else {
+        touchCancel(event);
+    }
 }
 
 function touchCancel(event) {
-	// reset the variables back to default values
-	fingerCount = 0;
-	startX = 0;
-	startY = 0;
-	curX = 0;
-	curY = 0;
-	deltaX = 0;
-	deltaY = 0;
-	horzDiff = 0;
-	vertDiff = 0;
-	swipeLength = 0;
-	swipeAngle = null;
-	swipeDirection = null;
-	triggerElementID = null;
-	var mobile_bar = document.getElementById("mobile_bar");
-	mobile_bar.style.backgroundColor = "#9AD2E4";
+    // reset the variables back to default values
+    fingerCount = 0;
+    startX = 0;
+    startY = 0;
+    curX = 0;
+    curY = 0;
+    deltaX = 0;
+    deltaY = 0;
+    horzDiff = 0;
+    vertDiff = 0;
+    swipeLength = 0;
+    swipeAngle = null;
+    swipeDirection = null;
+    triggerElementID = null;
+    var mobile_bar = document.getElementById("mobile_bar");
+    mobile_bar.style.backgroundColor = "#9AD2E4";
 }
-	
+
 function caluculateAngle() {
-	var X = startX-curX;
-	var Y = curY-startY;
-	var Z = Math.round(Math.sqrt(Math.pow(X,2)+Math.pow(Y,2))); //the distance - rounded - in pixels
-	var r = Math.atan2(Y,X); //angle in radians (Cartesian system)
-	swipeAngle = Math.round(r*180/Math.PI); //angle in degrees
-	if ( swipeAngle < 0 ) { swipeAngle =  360 - Math.abs(swipeAngle); }
+    var X = startX-curX;
+    var Y = curY-startY;
+    var Z = Math.round(Math.sqrt(Math.pow(X,2)+Math.pow(Y,2))); //the distance - rounded - in pixels
+    var r = Math.atan2(Y,X); //angle in radians (Cartesian system)
+    swipeAngle = Math.round(r*180/Math.PI); //angle in degrees
+    if ( swipeAngle < 0 ) { swipeAngle =  360 - Math.abs(swipeAngle); }
 }
-	
+
 function determineSwipeDirection() {
-	if ( (swipeAngle <= 45) && (swipeAngle >= 0) ) {
-		swipeDirection = 'left';
-	} else if ( (swipeAngle <= 360) && (swipeAngle >= 315) ) {
-		swipeDirection = 'left';
-	} else if ( (swipeAngle >= 135) && (swipeAngle <= 225) ) {
-		swipeDirection = 'right';
-	} else if ( (swipeAngle > 45) && (swipeAngle < 135) ) {
-		swipeDirection = 'down';
-	} else {
-		swipeDirection = 'up';
-	}
+    if ( (swipeAngle <= 45) && (swipeAngle >= 0) ) {
+        swipeDirection = 'left';
+    } else if ( (swipeAngle <= 360) && (swipeAngle >= 315) ) {
+        swipeDirection = 'left';
+    } else if ( (swipeAngle >= 135) && (swipeAngle <= 225) ) {
+        swipeDirection = 'right';
+    } else if ( (swipeAngle > 45) && (swipeAngle < 135) ) {
+        swipeDirection = 'down';
+    } else {
+        swipeDirection = 'up';
+    }
 }
-	
+
 
 
 // swipe left to hide sidebar
@@ -144,13 +145,13 @@ function processingRoutine() {
     var swipedElement = document.getElementById(triggerElementID);
     if (swipeDirection == 'left') {
         if (triggerElementID=='infobar_mobile') {
-        	hideInfo();
-        	}
+            hideInfo();
+        }
         else if (triggerElementID=='mobile_bar') 
-        	shiftDate(1);
+            trigger(1);
     } else if (swipeDirection == 'right')
-    	if (triggerElementID=='mobile_bar') 
-    		shiftDate(-1);
+        if (triggerElementID=='mobile_bar') 
+            trigger(-1);
     /*
     else if (swipeDirection == 'left' && triggerElementID == 'boxes') {
     	shiftDate(1);
@@ -158,4 +159,39 @@ function processingRoutine() {
     else if (swipeDirection == 'right' && triggerElementID == 'boxes') {
     	shiftDate(-1);
     } */
+}
+
+function trigger(val){
+    if(val == 1) { // swipe left, go to next day
+        var firstPos = window.innerWidth * -1.5;
+        var offScreen = "150vw";
+    } else if(val == -1){
+        //TODO: HANDLE CASE WHEN ITS TODAY
+        // i.e. cant move back
+        var firstPos = window.innerWidth * 1.5;
+        var offScreen = "-150vw";
+    }
+            
+    $( "#mobile_bar" ).animate({
+        backgroundColor: "#7E95BA"
+        }, 250, function(){
+            // SET back to original color
+            $( "#mobile_bar" ).animate({
+                backgroundColor: "#9AB7E4"
+            }, 250, null);
+        });
+    
+    // TWO ANIMATIONS HAPPEN AT THE SAME TIME
+    $( "#date_display" ).animate({
+        marginLeft: firstPos
+    }, 250, function() {
+        // Animation complete.
+        $( "#date_display" ).css("margin-left", offScreen);
+        shiftDate(val)
+        $( "#date_display" ).animate({
+            marginLeft: 0
+        }, 250, function(){
+            //MOVE back into center
+        });
+    });
 }
